6bf5a6970125093751853e0a48aab34adf9147e1
Jeff <982855239@qq.com>
2020-07-27 21:30:38 +0800

    menu第一版

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
-    <div className="App" style={{marginTop: '10px'}}>
+    <div className="App" style={{ marginTop: "10px" }}>
       <header className="App-header">
-        <Button disabled >hello button</Button>
-        <Button onClick={(e)=>alert(e.target)} btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
-        <Button btnType={ButtonType.Link} href='http://www.baidu.com'>Link</Button>
-        <Button btnType={ButtonType.Link} disabled href='http://www.baidu.com'>Disabled Link</Button>
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0}>cool link1</MenuItem>
+          <MenuItem index={1}>cool link2</MenuItem>
+        </Menu>
+        <Button disabled>hello button</Button>
+        <Button
+          onClick={(e) => alert(e.target)}
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        >
+          Large Primary
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          Small Danger
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+          Link
+        </Button>
+        <Button btnType={ButtonType.Link} disabled href="http://www.baidu.com">
+          Disabled Link
+        </Button>
       </header>
     </div>
   );
