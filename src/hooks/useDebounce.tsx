182a4d57bc66fa76915f7ca0cb1705130771a74d
Jeff <982855239@qq.com>
2020-09-26 13:33:18 +0800

    添加注释

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// 自定义hooks-防抖
 function useDebounce(value: any, delay = 300) {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
