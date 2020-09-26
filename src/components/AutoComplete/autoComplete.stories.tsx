427e17a5c42e19fa6ca6cc04e7692efe06d62e00
Jeff <982855239@qq.com>
2020-09-26 13:18:17 +0800

    添加键盘事件

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -45,7 +45,7 @@ const simpleComplete = () => {
     const itemWithGithub = item as DataSourceType<GithubUserProps>;
     return (
       <>
-        <h2>Name: {itemWithGithub.login}</h2>
+        <h2>Name: {itemWithGithub.value}</h2>
         <p>url: {itemWithGithub.url}</p>
       </>
     );
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,9 +1,17 @@
-import React, { FC, useState,useEffect, ChangeEvent, ReactElement } from "react";
+import React, {
+  FC,
+  useState,
+  useEffect,
+  ChangeEvent,
+  KeyboardEvent,
+  ReactElement,
+} from "react";
+import classNames from "classnames";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Input, { InputProps } from "../Input/input";
 import Icon from "../Icon/icon";
-import useDebounce from '../../hooks/useDebounce';
+import useDebounce from "../../hooks/useDebounce";
 library.add(fas);
 interface DataSourceObject {
   value: string;
@@ -29,25 +37,53 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
   const [inputValue, setInputValue] = useState(value);
   const [loading, setLoading] = useState(false);
-
-  const debouncedValue = useDebounce(inputValue,500);
+  const [highlightIndex, setHighlightIndex] = useState(-1);
+  const debouncedValue = useDebounce(inputValue, 500);
   useEffect(() => {
     if (debouncedValue) {
-        const result = fetchSuggestions(debouncedValue);
-        if (result instanceof Promise) {
-          console.log("triggered");
-          setLoading(true);
-          result.then((data) => {
-            setLoading(false);
-            setSuggestions(data);
-          });
-        } else {
-          setSuggestions(result);
-        }
+      const result = fetchSuggestions(debouncedValue);
+      if (result instanceof Promise) {
+        console.log("triggered");
+        setLoading(true);
+        result.then((data) => {
+          setLoading(false);
+          setSuggestions(data);
+        });
       } else {
-        setSuggestions([]);
+        setSuggestions(result);
       }
+    } else {
+      setSuggestions([]);
+    }
+    setHighlightIndex(-1);
   }, [debouncedValue]);
+  const highlight = (index: number) => {
+    if (index < 0) index = 0;
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1;
+    }
+    setHighlightIndex(index);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex]);
+        }
+        break;
+      case 38:
+        highlight(highlightIndex - 1);
+        break;
+      case 40:
+        highlight(highlightIndex + 1);
+        break;
+      case 27:
+        setSuggestions([]);
+        break;
+      default:
+        break;
+    }
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim();
@@ -69,9 +105,13 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     return (
       <ul>
         {suggestions.map((item, index) => {
+          const cnames = classNames("suggestion-item", {
+            "item-highlighted": index === highlightIndex,
+          });
           return (
             <li
               key={index}
+              className={cnames}
               onClick={() => {
                 handleSelect(item);
               }}
@@ -85,7 +125,12 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   };
   return (
     <div className="viking-auto-complete">
-      <Input onChange={handleChange} value={inputValue} {...restProps} />
+      <Input
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        value={inputValue}
+        {...restProps}
+      />
       {loading && (
         <ul>
           <Icon icon="spinner" spin />
