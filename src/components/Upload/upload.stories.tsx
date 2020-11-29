dfaa09b32d4f18a1da562180a311a7447b3f5ccb
Jeff <982855239@qq.com>
2020-11-29 10:22:04 +0800

    @Jeff:add beforeupload and update code format

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -4,10 +4,25 @@ import {action} from '@storybook/addon-actions';
 import {Upload} from './upload';
 import React from 'react';
 
+const checkFileSize = (file:File) => {
+    if(Math.round(file.size / 1024) > 50) {
+        alert('file is so big');
+        return false;
+    }
+    return true;
+}
+
+const filePromise = (file:File) => {
+    const newFile = new File([file],'new_name.xls', {type:file.type});
+    return Promise.resolve(newFile)
+}
+
 const simpleUpload = () => {
     return (
         <Upload
-         action="https://www.mocky.io/v2/5185415ba171ea3a00704eed/posts/"
+         action="https://jsonplaceholder.typicode.com/posts/"
+         onChange={action('changed')}
+         beforeUpload={filePromise}
         />
     )
 }
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,74 +1,107 @@
-import React, {FC,useRef, ChangeEvent} from 'react';
-import axios from 'axios';
-import Button from '../Button/button';
+import React, { FC, useRef, ChangeEvent } from "react";
+import axios from "axios";
+import Button from "../Button/button";
 
 export interface UploadProps {
-    action: string;
-    onProgress?: (percentage:number,file:File) => void;
-    onSuccess?: (data:any,file:File) => void;
-    onError?: (err:any,file:File) => void;
+  action: string;
+  beforeUpload?: (file: File) => boolean | Promise<File>;
+  onProgress?: (percentage: number, file: File) => void;
+  onSuccess?: (data: any, file: File) => void;
+  onError?: (err: any, file: File) => void;
+  onChange?: (file: File) => void;
 }
 
-export const Upload:FC<UploadProps> = (props) => {
-    const {action,onProgress,onSuccess,onError} = props;
-    const fileInput = useRef<HTMLInputElement>(null);
-    const handleClick = () => {
-        if(fileInput.current) {
-            fileInput.current.click();
-        }
+export const Upload: FC<UploadProps> = (props) => {
+  const {
+    action,
+    beforeUpload,
+    onProgress,
+    onSuccess,
+    onError,
+    onChange,
+  } = props;
+  const fileInput = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    if (fileInput.current) {
+      fileInput.current.click();
     }
-    const handleFileChange = (e:ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files;
-        if(!file) {
-            return;
-        }
-        uploadFiles(file);
-        if(fileInput.current) {
-            fileInput.current.value = '';
-        }
+  };
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files;
+    if (!file) {
+      return;
     }
-    const uploadFiles = (files:FileList) => {
-        let postFiles = Array.from(files);
-        postFiles.forEach(file => {
-            const formData = new FormData();
-            formData.append(file.name,file);
-            axios.post(action, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                },
-                onUploadProgress: (e) => {
-                    let percentage = Math.round((e.loaded * 100) / e.total) || 0;
-                    if(percentage < 100) {
-                        if(onProgress) {
-                            onProgress(percentage,file);
-                        }
-                    }
-                }
-            }).then(res => {
-                console.log(res);
-                if(onSuccess) {
-                    onSuccess(res.data,file);
-                }
-            }).catch(err => {
-                console.log(err);
-                if(onError) {
-                    onError(err,file);
-                }
-            })
-        })
+    uploadFiles(file);
+    if (fileInput.current) {
+      fileInput.current.value = "";
     }
-    return (
-        <div className='viking-upload-component'>
-            <Button 
-               btnType='primary'
-               onClick={handleClick}
-            >Upload File</Button>
-            <input type="file" className='viking-file-input'
-               style={{display:'none'}}
-               ref={fileInput}
-               onChange={handleFileChange}
-            />
-        </div>
-    )
-}
+  };
+  const uploadFiles = (files: FileList) => {
+    let postFiles = Array.from(files);
+    postFiles.forEach((file) => {
+      if (!beforeUpload) {
+        post(file);
+      } else {
+        const result = beforeUpload(file);
+        if (result && result instanceof Promise) {
+          result.then((processedFile) => {
+            post(processedFile);
+          });
+        } else if (result !== false) {
+          post(file);
+        }
+      }
+    });
+  };
+  const post = (file: File) => {
+    const formData = new FormData();
+    formData.append(file.name, file);
+    axios
+      .post(action, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (e) => {
+          let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+          if (percentage < 100) {
+            if (onProgress) {
+              onProgress(percentage, file);
+            }
+          }
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        if (onSuccess) {
+          onSuccess(res.data, file);
+        }
+        if(onChange) {
+            onChange(file);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        if (onError) {
+          onError(err, file);
+        }
+        if(onChange) {
+            onChange(file);
+        }
+      });
+  };
+  return (
+    <div className="viking-upload-component">
+      <Button btnType="primary" onClick={handleClick}>
+        Upload File
+      </Button>
+      <input
+        type="file"
+        className="viking-file-input"
+        style={{ display: "none" }}
+        ref={fileInput}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+};
 export default Upload;
